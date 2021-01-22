fde83a458e0818a99d92b255c7e07cc59e33c62b
Hu <Hu@ThinkPad-T430>
2021-01-21 19:24:04 -0500

    add password based encryption support

--- japp-se-common/src/test/java/com/itdevcloud/japp/se/common/test/SecurityUtilTest.java
+++ japp-se-common/src/test/java/com/itdevcloud/japp/se/common/test/SecurityUtilTest.java
@@ -11,7 +11,6 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
-import com.itdevcloud.japp.se.common.security.Crypter;
 import com.itdevcloud.japp.se.common.security.EncryptedInfo;
 import com.itdevcloud.japp.se.common.util.FileUtil;
 import com.itdevcloud.japp.se.common.util.SecurityUtil;
@@ -35,7 +34,7 @@ public class SecurityUtilTest  {
 	@Test
 	public void encryptText_decryptTextTest() {
 		String clearText1 = "Hello World!";
-		EncryptedInfo encryptedInfo = SecurityUtil.encrypt(clearText1);
+		EncryptedInfo encryptedInfo = SecurityUtil.encrypt(clearText1, null);
 		System.out.println(""+encryptedInfo);
 		if(encryptedInfo == null || StringUtil.isEmptyOrNull(encryptedInfo.getEncryptedText())) {
 			assertFalse(true, "encryptedInfo is empty");
@@ -43,13 +42,32 @@ public class SecurityUtilTest  {
 		String clearText2 = SecurityUtil.decrypt(encryptedInfo.getEncryptionKey(), encryptedInfo.getEncryptedText());
 		assertEquals(clearText1, clearText2);
 	}
+	
+	//@Test
+	public void encryptTextPwd_decryptPwdTextTest() {
+		String clearText1 = "Hello World!";
+		String pwd = "password12345";
+		String salt = "12345";
+		String encodedKey = SecurityUtil.generateKeyFromPassword(pwd, salt, null);
+		
+		EncryptedInfo encryptedInfo = SecurityUtil.encrypt(clearText1, encodedKey);
+		
+		System.out.println(""+encryptedInfo);
+		if(encryptedInfo == null || StringUtil.isEmptyOrNull(encryptedInfo.getEncryptedText())) {
+			assertFalse(true, "encryptedInfo is empty");
+		}
+		
+		String encodedKey2 = SecurityUtil.generateKeyFromPassword(pwd, salt, null);
+		String clearText2 = SecurityUtil.decrypt(encodedKey2, encryptedInfo.getEncryptedText());
+		assertEquals(clearText1, clearText2);
+	}
 
-	@Test
+	//@Test
     public void encryptFileToString_decryptStringTest() {
 
         String inputfileName = "/Test-1.txt";
 		
-		EncryptedInfo encryptedInfo = SecurityUtil.encryptFile(inputfileName);
+		EncryptedInfo encryptedInfo = SecurityUtil.encryptFile(inputfileName, null);
 		System.out.println(""+encryptedInfo);
 		
 		String decryptedContent = SecurityUtil.decrypt(encryptedInfo.getEncryptionKey(), encryptedInfo.getEncryptedText());
@@ -59,14 +77,35 @@ public class SecurityUtilTest  {
 		assertTrue(decryptedContent.startsWith("Test-1"), "decrypted text start with 'Test-1");
 
     }
+    
+	//@Test
+    public void encryptFileToStringPwd_decryptStringPwdTest() {
+
+        String inputfileName = "/Test-1.txt";
+		
+        String pwd = "password12345";
+		String salt = "12345";
+		String encodedKey = SecurityUtil.generateKeyFromPassword(pwd, salt, null);
+		
+		EncryptedInfo encryptedInfo = SecurityUtil.encryptFile(inputfileName, encodedKey);
+		System.out.println(""+encryptedInfo);
+		
+		String encodedKey2 = SecurityUtil.generateKeyFromPassword(pwd, salt, null);
+		String decryptedContent = SecurityUtil.decrypt(encodedKey2, encryptedInfo.getEncryptedText());
+		
+		System.out.println("---Decrypted content---\n"+decryptedContent);
+		
+		assertTrue(decryptedContent.startsWith("Test-1"), "decrypted text start with 'Test-1");
+
+    }
 	
-	@Test
+	//@Test
     public void encryptFileToFile_decryptFileTest() {
 
         String inputfileName = "/Test-1.txt";
         String outputfileName = "Test-1-encrypted.txt";
 		
-		EncryptedInfo encryptedInfo = SecurityUtil.encryptFile(inputfileName, outputfileName);
+		EncryptedInfo encryptedInfo = SecurityUtil.encryptFile(inputfileName, outputfileName, null);
 		
 		String decryptedContent = SecurityUtil.decryptFile(encryptedInfo.getEncryptionKey(), outputfileName);
 		
@@ -77,16 +116,61 @@ public class SecurityUtilTest  {
 		//assertTrue(true);
 
     }
+	//@Test
+    public void encryptFileToFilePwd_decryptFilePwdTest() {
+
+        String inputfileName = "/Test-1.txt";
+        String outputfileName = "Test-1-encrypted.txt";
+		
+        String pwd = "password12345";
+		String salt = "12345";
+		String encodedKey = SecurityUtil.generateKeyFromPassword(pwd, salt, null);
+
+		EncryptedInfo encryptedInfo = SecurityUtil.encryptFile(inputfileName, outputfileName, encodedKey);
+		
+		String encodedKey2 = SecurityUtil.generateKeyFromPassword(pwd, salt, null);
+		String decryptedContent = SecurityUtil.decryptFile(encodedKey2, outputfileName);
+		
+		System.out.println("---Decrypted content---\n"+decryptedContent);
+		
+		assertTrue(decryptedContent.startsWith("Test-1"), "decrypted text start with 'Test-1");
+
+    }
 	
-	@Test
+	//@Test
     public void encryptFileToFile_decryptFileToFileTest() {
 
         String inputfileName = "/Test-1.txt";
         String encryptedOutputfileName = "Test-1-encrypted.txt";
         String decryptedOutputfileName = "Test-1-decrypted.txt";
 		
-		EncryptedInfo encryptedInfo = SecurityUtil.encryptFile(inputfileName, encryptedOutputfileName);
+		EncryptedInfo encryptedInfo = SecurityUtil.encryptFile(inputfileName, encryptedOutputfileName, null);
 		SecurityUtil.decryptFile(encryptedInfo.getEncryptionKey(), encryptedOutputfileName, decryptedOutputfileName);
+		
+		String decryptedContent = FileUtil.getFileContentAsString(decryptedOutputfileName);
+		
+		System.out.println("---Decrypted content---\n"+decryptedContent);
+
+		assertTrue(decryptedContent.startsWith("Test-1"), "decrypted text start with 'Test-1");
+
+		//assertTrue(true);
+
+    }
+	//@Test
+    public void encryptFileToFilePwd_decryptFileToFilePwdTest() {
+
+        String inputfileName = "/Test-1.txt";
+        String encryptedOutputfileName = "Test-1-encrypted.txt";
+        String decryptedOutputfileName = "Test-1-decrypted.txt";
+		
+        String pwd = "password12345";
+		String salt = "12345";
+		String encodedKey = SecurityUtil.generateKeyFromPassword(pwd, salt, null);
+
+		EncryptedInfo encryptedInfo = SecurityUtil.encryptFile(inputfileName, encryptedOutputfileName, encodedKey);
+
+		String encodedKey2 = SecurityUtil.generateKeyFromPassword(pwd, salt, null);
+        SecurityUtil.decryptFile(encodedKey2, encryptedOutputfileName, decryptedOutputfileName);
 		
 		String decryptedContent = FileUtil.getFileContentAsString(decryptedOutputfileName);
 		
